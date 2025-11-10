4dc0c20c553adc76ac23401977dc1ac387eea8f4
Anizoomy <habeebolayemi518@gmail.com>
2025-11-10 11:19:47 +0100

    Swagger Done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT;
 const cors = require('cors');
+const setupSwagger = require('./swagger/swagger');
 const weatherRoutes = require('./routes/weatherRouter');
 
 const app = express();
@@ -10,12 +11,14 @@ app.use(express.json());
 
 app.use(cors());
 
+setupSwagger(app);
+
 app.use('/api/v1', weatherRoutes);
 
 app.use('/', (req, res) => {
-  res.send('Welcome To My Weather App');
+  res.send('Welcome to SkyPulse weather API');
 });
 
 app.listen(PORT, () => {
-    console.log(`server running on http://localhost: ${PORT}`);
+    console.log(`server running on http://localhost:${PORT}`, `and https://skypulse-g8pk.onrender.com`);
 })
